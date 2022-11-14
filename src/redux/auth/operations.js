52431190d8e9af6f0a8e4fe4ebc66cc008da0d19
AleksandrTikka <alexandertikka@gmail.com>
2022-11-14 23:29:07 +0200

    add logOut reducer

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -4,13 +4,15 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
 // Utility to add JWT
-const setAuthHeader = token => {
-  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-};
+const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
 
-// Utility to remove JWT
-const clearAuthHeader = () => {
-  axios.defaults.headers.common.Authorization = '';
+  // Utility to remove JWT
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
 };
 
 /*
@@ -21,7 +23,7 @@ const register = createAsyncThunk('auth/register', async credentials => {
   try {
     const { data } = await axios.post('/users/signup', credentials);
     // After successful registration, add the token to the HTTP header
-    setAuthHeader(data.token);
+    token.set(data.token);
     return data;
   } catch (error) {}
 });
@@ -34,16 +36,20 @@ const logIn = createAsyncThunk('auth/login', async credentials => {
   try {
     const { data } = await axios.post('/users/login', credentials);
     // After successful login, add the token to the HTTP header
-    setAuthHeader(data.token);
+    token.set(data.token);
     return data;
   } catch (error) {}
 });
 
+/*
+ * POST @ /users/logout
+ * headers: Authorization: Bearer token
+ */
 export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
   try {
     await axios.post('/users/logout');
     // After a successful logout, remove the token from the HTTP header
-    clearAuthHeader();
+    token.unset();
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
   }
@@ -67,7 +73,7 @@ export const refreshUser = createAsyncThunk(
 
     try {
       // If there is a token, add it to the HTTP header and perform the request
-      setAuthHeader(persistedToken);
+      token.set(persistedToken);
       const res = await axios.get('/users/me');
       return res.data;
     } catch (error) {
